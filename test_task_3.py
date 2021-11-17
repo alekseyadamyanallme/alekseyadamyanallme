7935136683f01066abd19fb6a97afa74282dd71e
alekseyadamyanallme <71122534+alekseyadamyanallme@users.noreply.github.com>
2021-11-17 21:50:33 +0400

    Update test_task_3.py

--- test_task_3.py
+++ test_task_3.py
@@ -1,12 +1,12 @@
 
 points = int(input()) #вводим количество баллов
-if points in range(1, 100):
+if points in range(1, 101):
     print('Скидка 1%')
-elif points in range(101, 200):
+elif points in range(101, 201):
     print('Скидка 3%')
-elif points in range(201, 500):
+elif points in range(201, 501):
     print('Скидка 5%')
-elif points >=500:
+elif points >=501:
     print('Скидка 10%')
 else:
     print('У Вас нет скидки')
